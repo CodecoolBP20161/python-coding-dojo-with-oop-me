9251e3aad333b0c26b80dc51b948b8c08c89b694
beru26 <berkes.edina@gmail.com>
2016-09-13 09:58:23 +0200

    reset is done

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 
 class Contact:
-    pass
+
+    all_contacts = []
+
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+
+        Contact.all_contacts.append(self)
+
+    @classmethod
+    def reset_contacts(cls):
+        for contact_object in cls.all_contacts:
+            return cls.all_contacts.remove(contact_object)
